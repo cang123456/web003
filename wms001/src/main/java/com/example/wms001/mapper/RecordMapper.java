7b513cb8194897a7242a0ffbf19dc0d1d860251b
buyaola <3167204891@qq.com>
2026-04-04 22:43:59 +0800

    commit，更新诗词模块，完善物品管理模块

---
+++ wms001/src/main/java/com/example/wms001/mapper/RecordMapper.java
@@ -0,0 +1,18 @@
+package com.example.wms001.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.wms001.entity.Record;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author buyaola
+ * @since 2026-04-04
+ */
+@Mapper
+public interface RecordMapper extends BaseMapper<Record> {
+
+}
---
+++ wms001/src/main/java/com/example/wms001/service/impl/RecordServiceImpl.java
@@ -0,0 +1,20 @@
+package com.example.wms001.service.impl;
+
+import com.example.wms001.entity.Record;
+import com.example.wms001.mapper.RecordMapper;
+import com.example.wms001.service.IRecordService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author buyaola
+ * @since 2026-04-04
+ */
+@Service
+public class RecordServiceImpl extends ServiceImpl<RecordMapper, Record> implements IRecordService {
+
+}
